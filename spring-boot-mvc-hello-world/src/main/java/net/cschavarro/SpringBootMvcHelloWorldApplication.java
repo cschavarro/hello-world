8a303d08a462a5520e0ac8f9ee476ae8fd0d6616
Steven <Steven@Steven-PC>
2016-10-09 20:24:21 -0500

    Add First Project

---
+++ spring-boot-mvc-hello-world/src/main/java/net/cschavarro/SpringBootMvcHelloWorldApplication.java
@@ -0,0 +1,12 @@
+package net.cschavarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcHelloWorldApplication.class, args);
+	}
+}
---
+++ spring-boot-mvc-hello-world/src/test/java/net/cschavarro/SpringBootMvcHelloWorldApplicationTests.java
@@ -0,0 +1,16 @@
+package net.cschavarro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMvcHelloWorldApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
