56d19e4fdfdf717710d293c0abe2a2a9613e60ac
Steven <Steven@Steven-PC>
2016-10-24 10:54:13 -0500

    Example of simple security with login form

---
+++ spring-boot-hello-world-security/src/main/java/net/cschavarro/config/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package net.cschavarro.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+			.withUser("bill").password("abc123").roles("USER")
+			.and()
+			.withUser("admin").password("root123").roles("ADMIN")
+			.and()
+			.withUser("dba").password("root123").roles("ADMIN","DBA");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/", "/home").permitAll()
+			.antMatchers("/admin/**").access("hasRole('ADMIN')")
+			.antMatchers("/dba/**").access("hasRole('ADMIN') and hasRole('DBA')")
+			.and().formLogin()
+			.and().exceptionHandling().accessDeniedPage("/Access_Denied");
+	}
+	
+}
